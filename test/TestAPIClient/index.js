55f0ae0555fc5d4eb56190f8a509b1181b1b7360
Dan Rumney <drumney@jellyvision.com>
2015-10-08 01:02:33 -0500

    Setup for Travis CI

---
+++ test/TestAPIClient/index.js
@@ -0,0 +1,40 @@
+var fixtures =  [
+    require('./companies-fixture'),
+    require('./people-fixture'),
+    require('./projects-fixture')
+    ];
+var _ = require('lodash');
+var xmlResponseHandler = require('../../src/lib/xmlResponseHandler');
+
+var endpoints = _.reduce(fixtures, function(endpoints, fixture) {
+    "use strict";
+    return _(endpoints).concat( fixture.getEndpoints()).value();
+}, []);
+
+module.exports = {
+    get: function (url, cb) {
+        "use strict";
+        var response;
+        console.log("GET: " + url);
+        var requestedEndpoint = _.find(endpoints, function(endpoint) {
+            return endpoint.matcher(url);
+        });
+        if(requestedEndpoint) {
+            response = requestedEndpoint.handler.call(requestedEndpoint, url);
+        }
+        xmlResponseHandler(null, { statusCode: response ? 200:404 }, response || "<error></error>", cb);
+
+    },
+    post: function (endpoint, cb) {
+        "use strict";
+        cb(new Error("Not yet implemented"));
+    },
+    put: function (endpoint, cb) {
+        "use strict";
+        cb(new Error("Not yet implemented"));
+    },
+    delete: function (endpoint, cb) {
+        "use strict";
+        cb(new Error("Not yet implemented"));
+    }
+};
