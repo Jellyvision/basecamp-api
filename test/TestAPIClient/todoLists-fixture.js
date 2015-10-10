855a2ad0699e1aed4b9bd6327df8919d6cd40905
Dan Rumney <drumney@jellyvision.com>
2015-10-10 10:52:09 -0500

    Cleaning up some stragglers...

--- test/TestAPIClient/todoLists-fixture.js
+++ test/TestAPIClient/todoLists-fixture.js
@@ -1,18 +1,30 @@
 var _ = require('lodash');
 var FixtureBuilder = require('./FixtureBuilder');
-
 var xmlify = require('./xmlify');
-var todoLists = require('./fixtures/todoLists.json');
 
-var data = {};
-_.forEach(todoLists, function ( todoList, id ) {
-    "use strict";
-    data[id] = xmlify('company',todoList);
-});
+
+var todoLists = require('./fixtures/todoLists.json');
+var todoListItems = require('./fixtures/todoListItems.json');
 
 var fixtureBuilder = new FixtureBuilder("todo-lists");
 
+var data = {};
+
 module.exports = fixtureBuilder
-    .setData({})
-    .addBaseEndpoints()
+    .setData(data)
+    .addEndpoint({
+        matcher: function (url) {
+            "use strict";
+            return url.match(new RegExp("/todo_lists.xml\?responsible_party=(\d*)"));
+        },
+        handler: function (url) {
+            "use strict";
+            var found = this.matcher(url);
+            if(this.data[found[1]]) {
+                return xmlify(this.singular, this.data[found[1]])   ;
+            } else {
+                return undefined;
+            }
+        }
+    })
     .build();
